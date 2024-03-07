1d2e02ed5c73959e916882ead7a7f3cfc9ba3944
Maximilien Bey <bey.maximilien@gmail.com>
2024-03-07 17:56:34 +0100

    Remove ConfigModule from MovieModule

--- src/user.module.ts
+++ src/user.module.ts
@@ -1,25 +1,12 @@
 import { CommonModule } from "@app/common"
 import { RmqModule } from "@app/common/rmq/rmq.module"
 import { Module } from "@nestjs/common"
-import { ConfigModule } from "@nestjs/config"
-import * as joi from "joi"
 
 import { UserController } from "./user.controller"
 import { UserService } from "./user.service"
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      validationSchema: joi.object({
-        DATABASE_URL: joi.string().required(),
-        RABBIT_MQ_URL: joi.string().required(),
-      }),
-      envFilePath: "./apps/user/.env",
-    }),
-    RmqModule,
-    CommonModule,
-  ],
+  imports: [RmqModule, CommonModule],
   controllers: [UserController],
   providers: [UserService],
 })
